85c087a6a5311b1c87bb33fd2cee69d507b5b1dd
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-17 20:32:29 -0300

    Fix: Site name typo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,13 @@
-import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
+import { AppBar, Button, Toolbar, Typography } from '@material-ui/core';
+import { SITE_NAME } from '../constants/Constants';
 
 export const NavBar = () => {
     return (
         <AppBar position="relative">
             <Toolbar>
                 <Typography variant="h6" color="inherit" noWrap>
-                    ThingVerse
+                    {SITE_NAME}
                 </Typography>
                 <Button variant={'contained'} color={'primary'}>
                     Login
