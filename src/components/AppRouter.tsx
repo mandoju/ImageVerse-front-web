312499fa6b0d9c895448969d094e31553226f5cc
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-18 14:56:47 -0300

    Add: upload page route

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import { RecentImagesPage } from '../pages/RecentImagesPage';
+import { UploadImagePage } from '../pages/UploadImagePage';
+
 export const AppRouter = () => {
     return (
         <Switch>
+            <Route path="/upload">
+                <UploadImagePage />
+            </Route>
             <Route path="/">
                 <RecentImagesPage />
             </Route>
---
+++ src/pages/UploadImagePage.tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+export const UploadImagePage = () => {
+    return (
+        <div>
+            <Typography>Upload Image</Typography>
+        </div>
+    );
+};
