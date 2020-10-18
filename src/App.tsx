27a19915d700919f85684491437fbc3513305f86
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-18 14:50:02 -0300

    Add: Upload Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { RecentImagesPage } from './pages/RecentImagesPage';
 import { CssBaseline } from '@material-ui/core';
