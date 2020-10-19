fa08c1ccfb26b2db5d90803b8c85cf78a10afcea
Jorge Rama Krsna Mandoju <JorgeRamaKrsna@gmail.com>
2020-10-19 00:08:31 -0300

    Add: Image size on modal

--- src/components/Images/ImageModal.tsx
+++ src/components/Images/ImageModal.tsx
@@ -10,14 +10,14 @@ import {
     Slide,
 } from '@material-ui/core';
 import { TransitionProps } from '@material-ui/core/transitions/transition';
-import React from 'react';
+import React, { ImgHTMLAttributes, useEffect, useState } from 'react';
 import CloseIcon from '@material-ui/icons/Close';
 import ClockIcon from '@material-ui/icons/AccessTime';
 import { Image } from '../../models/Image';
 import AccountCircle from '@material-ui/icons/AccountCircle';
+import ImageSizeIcon from '@material-ui/icons/PhotoSizeSelectLarge';
 import moment from 'moment';
 import useWindowDimensions from '../../hooks/useWindowDimensions';
-
 interface ImageModalProps {
     image?: Image;
     handleClose: () => void;
@@ -26,6 +26,10 @@ interface ImageModalProps {
 export const ImageModal = ({ image, handleClose }: ImageModalProps) => {
     const classes = useStyles();
     const { height } = useWindowDimensions();
+    const [imageWidth, setImageWidth] = useState(0);
+    const [imageHeight, setImageHeigth] = useState(0);
+    const imgElement = React.useRef<HTMLImageElement>(null);
+
     return (
         <Dialog maxWidth={'lg'} open={!!image} onClose={handleClose} TransitionComponent={Transition}>
             <div>
@@ -49,6 +53,12 @@ export const ImageModal = ({ image, handleClose }: ImageModalProps) => {
                             <AccountCircle />
                             <Typography className={classes.userNameText}>{image?.User.name}</Typography>
                         </div>
+                        <div className={classes.iconTextContainer}>
+                            <ImageSizeIcon />
+                            <Typography className={classes.userNameText}>
+                                {imageWidth}x{imageHeight}
+                            </Typography>
+                        </div>
                     </Toolbar>
                 </AppBar>
                 <div className={classes.imageContainer}>
@@ -57,7 +67,15 @@ export const ImageModal = ({ image, handleClose }: ImageModalProps) => {
                         src={image?.url}
                         width={'auto'}
                         height={'auto'}
+                        ref={imgElement}
                         style={{ maxHeight: height - 200, maxWidth: '100%' }}
+                        onLoad={() => {
+                            const loadImg = imgElement;
+                            if (loadImg && loadImg.current) {
+                                setImageWidth(loadImg.current.naturalWidth);
+                                setImageHeigth(loadImg.current.naturalHeight);
+                            }
+                        }}
                     />
                 </div>
             </div>
